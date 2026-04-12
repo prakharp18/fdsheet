d790ba6b1db24de1d8e825422314524da3015ac1
prakharp18 <pporwal2019@gmail.com>
2026-04-12 12:14:26 +0530

    inc seo/ update faulty url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://prakharp18.github.io',
+  url: 'https://fdsheet.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
